9c13de7689de65af9adfcc76989b7c7a5f46aebe
Toltecon <147872448+Toltecon@users.noreply.github.com>
2026-01-17 13:31:46 +0300

    Update PiramTrap.sol

--- src/PiramTrap.sol
+++ src/PiramTrap.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED                        
+// SPDX-License-Identifier: UNLICENSED                    
 pragma solidity ^0.8.13;                
   
 import {IERC20} from "forge-std/interfaces/IERC20.sol";            

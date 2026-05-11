289609efa11f082ef257666665f40dfe96e4ffdf
Toltecon <147872448+Toltecon@users.noreply.github.com>
2026-05-11 12:14:49 +0300

    Update PiramTrap.sol
    xc

--- src/PiramTrap.sol
+++ src/PiramTrap.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED                  
-       
+        
 import {IERC20} from "forge-std/interfaces/IERC20.sol";                       
            
 interface ITrap {    
